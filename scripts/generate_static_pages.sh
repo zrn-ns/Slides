c12394497a6e925bec79b8a359a15f5c2f90e7c5
Yuki Nagao <5319256+zrn-ns@users.noreply.github.com>
2021-05-10 13:14:18 +0900

    ADD Auto-generate script

---
+++ scripts/generate_static_pages.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd `dirname $0`
+
+cd ..
+
+if !(type "reveal-md" > /dev/null 2>&1); then
+    echo "Error: reveal-md is not installed!"
+    echo "Please install with below command:"
+    echo "$ npm install -g reveal-md"
+    echo ""
+    exit 1
+fi
+
+reveal-md content/ --static docs --static-dirs=content/attachments
+git add .
+git commit -m "ADD Generated static pages: `date`"
+
+cd scripts
